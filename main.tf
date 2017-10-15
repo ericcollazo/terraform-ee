8ec4966cb0fe99e9ee911a512909f146f2da0dd6
Eric Collazo <ericcollazo67@gmail.com>
2017-10-15 16:43:30 -0400

    Update key. Added missing key script.

--- main.tf
+++ main.tf
@@ -2,11 +2,15 @@ provider "digitalocean" {
   token = "${var.do_token}"
 }
 
+resource "digitalocean_ssh_key" "default" {
+  name = "${var.do_key_name}"
+  public_key = "${file(var.public_key_path)}"
+}
 resource "digitalocean_droplet" "manager-0" {
   name = "manager-0"
   region = "nyc3"
   size = "2gb"
-  image = "centos-7-x64"
+  image = "ubuntu-16-04-x64"
   ssh_keys = ["${digitalocean_ssh_key.default.id}"]
   private_networking = true
 
