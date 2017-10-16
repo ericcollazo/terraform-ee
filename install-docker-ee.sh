bc1ba52675649d1066ea5a6c73d3d44e40c97e25
Eric Collazo <ericcollazo67@gmail.com>
2017-10-15 21:28:36 -0400

    Install EE working!

--- install-docker-ee.sh
+++ install-docker-ee.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
-sudo apt-get update
+sudo apt-get update -y
 
-sudo apt-get install \
+sudo apt-get install -y \
     apt-transport-https \
     ca-certificates \
     curl \
     software-properties-common
 
-# curl -fsSL https://storebits.docker.com/ee/ubuntu/sub-fe7c336e-6219-4c02-bd49-7321f64f107f/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://storebits.docker.com/ee/ubuntu/sub-fe7c336e-6219-4c02-bd49-7321f64f107f/ubuntu/gpg | sudo apt-key add -
 
-# sudo add-apt-repository \
-#    "deb [arch=amd64] https://storebits.docker.com/ee/ubuntu/sub-fe7c336e-6219-4c02-bd49-7321f64f107f/ubuntu \
-#    $(lsb_release -cs) \
-#    stable-17.06"
+sudo add-apt-repository \
+   "deb [arch=amd64] https://storebits.docker.com/ee/ubuntu/sub-fe7c336e-6219-4c02-bd49-7321f64f107f/ubuntu \
+   $(lsb_release -cs) \
+   stable-17.06"
 
-# sudo apt-get update
+sudo apt-get update -y
 
-# sudo apt-get install docker-ee
-
-# sudo docker run hello-world
+sudo apt-get install docker-ee -y
